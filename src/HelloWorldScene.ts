267a6ee5df9e9565710aceeaad9a30b26dc9bb1c
Vico Gimenez <gimenez.v@gmail.com>
2024-01-20 18:36:24 -0300

    Upgrading Phaser3 version from 3.55 to 3.70 since the latest documentation and examples are all 3.70.

--- src/HelloWorldScene.ts
+++ src/HelloWorldScene.ts
@@ -14,22 +14,21 @@ export default class HelloWorldScene extends Phaser.Scene {
 	}
 
 	create() {
-		this.add.image(400, 300, 'sky')
 
-	    const particles = this.add.particles('red')
+	    this.add.image(400, 300, 'sky')
 
-	    const emitter = particles.createEmitter({
+	    const emitter = this.add.particles(0, 0, 'red', {
 			speed: 100,
 			scale: { start: 1, end: 0 },
 			blendMode: 'ADD',
-		})
+	    })
 
-		const logo = this.physics.add.image(400, 100, 'logo')
+	    const logo = this.physics.add.image(400, 100, 'logo')
+	    
+	    logo.setVelocity(100, 200)
+	    logo.setBounce(1, 1)
+	    logo.setCollideWorldBounds(true)
 
-		logo.setVelocity(100, 200)
-		logo.setBounce(1, 1)
-		logo.setCollideWorldBounds(true)
-
-		emitter.startFollow(logo)
+	    emitter.startFollow(logo)
 	}
 }
