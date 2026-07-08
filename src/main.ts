e0b5bd2873ea2434baa87ed002d28455bb2a1ca5
Rodrigo Bozza <eu@rodrigobozza.com>
2026-07-08 08:54:11 -0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
-import ShaderScene from './ShaderScene'
-import TestShader from './TestShader'
 import Game from './scenes/Game'
 import UI from './scenes/UI'
 
 import SadnessPipeline from './scenes/SadnessShader.js'
-import GrayScalePipeline from './scenes/GrayScale.js'
-import BlurPostFX from './scenes/BlurPostFX.js'
 
 const config: Phaser.Types.Core.GameConfig = {
         type: Phaser.WEBGL,
@@ -20,7 +15,7 @@ const config: Phaser.Types.Core.GameConfig = {
                 matter: { debug: false }
 		},
     scene: [Game,UI],
-    pipeline: { 'sad': SadnessPipeline, 'gray': GrayScalePipeline, 'blur': BlurPostFX }
+    pipeline: { 'sad': SadnessPipeline }
 }
 
 export default new Phaser.Game(config)
